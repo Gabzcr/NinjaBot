c476427fb5287f924d77831ba69f0b645d75e955
Gabrielle Pauvert <gabrielle.pauvert@laposte.net>
2019-03-13 22:43:00 +0100

    Add !roll command

--- ninjaBot.py
+++ ninjaBot.py
@@ -1,6 +1,9 @@
 import discord
 import asyncio
 import os
+import random
+import re
+
 from discord.ext import commands
 bot = commands.Bot(command_prefix='!', description='your description')
 
@@ -51,6 +54,50 @@ async def join(ctx):
     await bot.delete_message(ctx.message)
 
 
+async def roll_g(ctx, l):
+    c = ctx.message.content[l+2:]
+    c2 = re.split("d|D", c)
+    try:
+        if len(c2) != 2:
+            raise(ValueError)
+        nb_of_dices = int(c2[0])
+        value_of_dices = int(c2[1])
+    except(ValueError):
+        await bot.say("{0.author.mention}, je ne reconnais pas cette expression.".format(ctx.message))
+        return()
+    results = []
+    if nb_of_dices <= 0:
+        await bot.say("{0.author.mention}, vous devez lancer au moins un dé.".format(ctx.message))
+        return()
+    if nb_of_dices > 4242:
+        await bot.say("{0.author.mention}, vous devez lancer moins de 4242 dés.".format(ctx.message))
+        return()
+    if value_of_dices <= 0:
+        await bot.say("{0.author.mention}, les dés doivent avoir une valeur au moins égale à 1.".format(ctx.message))
+        return()
+    if value_of_dices > 2**42:
+        await bot.say("{0.author.mention}, les dés doivent avoir une valeur inférieure à 2 puissance 42.".format(ctx.message))
+        return()
+    for dice in range(nb_of_dices):
+        results.append(random.randint(1,value_of_dices))
+    if nb_of_dices == 1 or nb_of_dices > 100:
+        await bot.say("{0.author.mention}, vous avez obtenu un ".format(ctx.message) + "**" + str(sum(results)) + "**" + ".")
+    else:
+        inter = "(" + str(results[0])
+        for value in results[1:]:
+            inter = inter + " + " + str(value)
+        inter = inter + ")"
+        await bot.say("{0.author.mention}, vous avez obtenu un ".format(ctx.message) + "**" + str(sum(results)) + "**" + " " + inter + ".")
+
+@bot.command(pass_context = True)
+async def roll(ctx):
+    await roll_g(ctx, 4)
+
+@bot.command(pass_context = True)
+async def r(ctx):
+    await roll_g(ctx, 1)
+
+
 @bot.listen()
 async def on_message(msg):
     if ':Ninja:' in msg.content:
@@ -66,6 +113,5 @@ async def on_reaction_add(reaction, user):
     if 'Ninja' in str(reaction.emoji):
         await asyncio.sleep(5)
         await bot.remove_reaction(reaction.message, reaction.emoji, user)
-
 
 bot.run(os.getenv('BOT_TOKEN'))
