cb7e637c2ee0d06a71f69ea36291441fc8be6fec
Gabrielle Pauvert <gabrielle.pauvert@laposte.net>
2019-04-01 22:36:44 +0200

    add easter eggs

---
+++ crocell_jokes.py
@@ -0,0 +1,141 @@
+C'est l'histoire d'un gars qui rentre dans un café. Plouf.
+
+C'est l'histoire d'un aveugle qui rentre dans un bar, puis dans une table, puis dans une chaise.
+
+Est-ce que t'as déjà vu la femme de Gilbert Montagné ? Lui non plus.
+
+Hey salut, t'as pas un 06 ? Parce que j'te trouve plutôt bot.
+
+Vous aviez déjà remarqué qu'un concierge, c'était un peu comme une bougie stupide ?
+
+Si un astronaute se blesse en orbite, est-ce que la blessure est sans gravité ?
+
+Les chats n'aiment pas l'eau. Parce que dans l'eau minet râle.
+
+Qu'est-ce qui est pire qu'un bébé dans une poubelle ? Un bébé dans deux poubelles.
+
+Quand un minable s'attaque à un autre minable, faut-il s'attendre à une guerre interminable ?
+
+Qu'est-ce qui est plus fort qu'un lion ? Deux lions. Et qu'est-ce qui est plus fort que deux lions ? Un dinosaure.
+
+HEY MAIS NINJA, ÇA RIME AVEC PYJAMA !
+
+Quelle est la différence entre un pigeon ?
+
+Quelle est la différence entre une grenouille et un tuyau d'arrosage ? Les deux sont en caoutchouc, sauf la grenouille.
+
+C'est l'histoire de papa tomate farcie, qui emmène les enfants tomate farcie dans la forêt. Les enfants tomates farcies sont tous contents de faire une balade avec leur papa tomate farcie. Alors le papa tomate farcie prend la voiture jusqu'à l'orée de la forêt, ils descendent à trois de la voiture et se mettent en route le long du sentier. Les arbres sont grands dans la forêt près du village tomate farcie. Mais le papa tomate farcie, fatigué par son travail, ne prête pas trop attention aux enfants tomate farcie qui jouent un peu derrière lui. Absorbé dans ses pensées, il lève soudain la tête et… les enfants tomates farcies ont disparu ! Alors le papa tomate farcie prend peur. Il regarde tout autour de lui en criant "Tomate farcie ! Tomate farcie !". Mais pas de réponse. Il décide de rebrousser chemin et de chercher les enfants tomates farcies le long du sentier. "Tomate farcie ! Tomate farcie !". Mais toujours pas de réponse. Il finit par tourner à la voiture, cherche autour, dedans, mais aucun signe des enfants tomates farcies. Vraiment en train de paniquer, il décide d’appeler maman tomate farcie, afin qu’elle vienne l’aider à trouver les enfants tomates farcies. Il monte donc dans la voiture et retourne chercher sa femme le plus rapidement possible. Après une bonne réprimande par Madame tomate farcie pour sa négligence, voilà que tous deux retournent finalement à l’orée de la forêt, et papa tomate farcie montre à maman tomate farcie le chemin qu’ils ont suivi. Mais toujours aucun signe des enfants tomates farcies. Les parents commencent à s’inquiéter sérieusement. Faut-il appeler la police tomate farcie ? Les gardes forestiers tomates farcies ? Mais sur le chemin de retour, non loin du parking, qui est-ce qu’ils retrouvent ? Les enfants tomates farcies ! Les parents tomates farcies sont furieux !
+« Mais où étiez-vous ? » s’écrie papa tomate farcie ?
+« Bah… On tomate farcie ! »
+…
+Ah non mince je me suis trompé, je reprend. C’est l’histoire d’un papa steak haché…
+
+Alors c'est Nietzsche qui s'insurge devant votre usage du terme de mauvaise blague, parce qu'une morale de ressentiment comme la vôtre devrait plutôt la traiter comme une méchante blague...
+
+Moi, pour faire de bonnes crillades, j'utilise du crocell.
+
+Moi je dis : le plus fort entre l’hippopotame et l’éléphant, c’est encore le rhinocéros
+
+Pourquoi les crabes marchent en diagonale ? Pour éviter les autres crabes qui marchent en diagonale.
+
+**Vous avez perdu au Jeu.**
+
+Comment fait-on pour aider sa colocataire à descendre d'un arbre ? On coupe la corde.
+
+Quel est le point commun entre l'humour noir et la nourriture ? Certains en ont, d'autres non.
+
+Qu'est-ce qui est schtroumpf et qui schtroumpfe quand on le schtroumpfe ? (Euh... un schtroumpf ?) Mais non, voyons : DEUX schtroumpfs !
+
+C'est Descartes qui est dans un bar. Le barman lui dit : « Et René, tu veux une blonde ? », il répond « Je ne pense pas », et il disparait.
+
+Comment faire aboyer un chat ? Le tremper dans l'essence et l'allumer : WOOOOOOOOOOF.
+
+Qu'est ce qui est vert et qui a des roues ? Un arbre, j'ai menti pour les roues.
+
+Heisenberg se fait arrêter par les flics. "Vous savez que vous roulez à 150km/h ?" "Ah ba c'est malin, je suis perdu maintenant..."
+
+Qu'est-ce qu'un ours polaire ? Un ours cartésien qui a changé de coordonnées.
+
+Soit epsilon < 0.
+
+C'est un bout de papier et il va se baigner... Mais il se noie car il a pas pied.
+
+Une bonne blague ? X² ... Oh, vous n'avez pas l'air d'aimer le second degré...
+
+Pourquoi les moutons ont parfois du mal à dormir ? Parce qu'ils ne peuvent compter que sur eux-même.
+
+Comment appelle-t-on un ascenseur au Japon ? En appuyant sur le bouton.
+
+Quelle est la différence entre un hamster et un TGV ? Le hamster ne comporte pas de vitres.
+
+C'est l'histoire de trois élus étudiants au CA.
+
+Qu'est-ce qui est tout rouge et qui tape à la vitre en hurlant ? Un bébé dans un micro-onde.
+
+Qu'est-ce qui est petit, carré et vert ? Un petit carré vert.
+
+C'est l'histoire d'une roulette russe avec un Beretta 9mm. PAN.
+
+Si un aviateur est un peu trop aventureux, doit-on lui demander de garder les pieds sur terre ?
+
+Un mineur peut-il réussir alors qu'il est au fond du trou ?
+
+Lorsqu'un allemand fait une blague pas drôle, est-ce voir le mal partout que dire qu'il a fait un four ?
+
+N'est-ce pas faire les choses à l'envers que d'avoir une illumination pour réparer une panne de courant ?
+
+Lorsqu'un soldat tire sur un allié en étant bourré, ne fait-il pas un tir ami saoul ?
+
+Un électricien que l'on appelle peut-il nous facturer la prise de contact ?
+
+Lorsqu'un pêcheur n'a encore attrapé aucun poisson depuis le début de la journée, est-ce que pour autant ses dernières espérances sont des truites ?
+
+Lorsque deux chimistes se bagarrent, n'ont-il pas plus de chance d'attraper un coup de blouse ?﻿
+
+Savais-tu que tu as un lien de parenté avec un nuage ? Ce nuage, c'est un cirrus. Si c'est russe, c'est slave. Si ça se lave, ça se nettoie. Et si ce n'est toi, c'est donc ton frère.
+
+Sur un pont arrive un car à fond, mais ses roues pètent. Du coup, le car casse et le pont pète. Le car tombe à l'eau et les roues coulent. Heureusement, le car nage et devient un car naval.
+
+Qu'est-ce qui est blanc et qui ne peut pas grimper aux arbres ? Un réfrigérateur.
+
+Un moine marche dans la savane. Il tombe nez-à-nez avec un lion affamé. Alors il s'agenouille, regarde vers le ciel et dit : « Seigneur, seigneur, faites que ce lion ait une pensée chrétienne pour moi ! ». Le lion s'agenouille, regarde vers le ciel, et s'écrie : « Seigneur, seigneur, bénissez ce repas ! »
+
+Finalement, privé d'épée, Baal ne vaut guère plus qu'une simple machine à laver : il n'est rien de plus qu'un Rince Démon.
+
+Comment appelle-t-on un démon qui aime faire la fête incognito ? Un Baal masqué.
+
+Faire des trêves, c'est important pour Blandin·e, surtout pour la santé, ça permet de faire de beaux rêves. (parce que trève sans t ça fait rêve)
+
+Si tu vois rouge, est ce que c'est parce que tu as l'alarme à l'oeil ?
+
+Je sens que l'ambiance est plutôt froide, alors je viens briser la glace !
+
+Il ne faut jamais juger les gens sur leurs fréquentations ; Judas, par exemple, avait des amis irréprochables.
+
+Monsieur et Madame Bricot ont un fils, comment s'appelle-t-il ? Judas.
+
+Prenez exemple sur Jésus : ça fait 2 000 ans qu'il n'a pas baissé les bras.
+
+Baal et Crocell sont chez Régis. Crocell fait une blague. La blague tombe à l'eau. Qu'est-ce qui reste ?
+
+Connaissez-vous la blague de Paf l'éléphant ? C'est un éléphant qui traverse la route, une voiture passe, et paf la voiture !
+
+Pourquoi les flamants roses dorment sur une jambe ? Parce que sur zéro jambe, ils tomberaient.
+
+Finalement, on peut dire que les chevaux, ils ont tout le temps l'estomac dans les talons
+
+C'est un schtroumpf qui court dans la forêt. Il court, il court, et BAM, il tombe et il se fait un bleu.
+
+Pourquoi c'est difficile de conduire dans le nord ? Parce que les voitures n'arrêtent pas de caler.
+
+J'ai une blague à propos d'un magasin, mais la dernière fois, elle n'a pas supermarché.
+
+Pourquoi est ce que les crocos finissent toujours en prison ? Parce que les crocos dealent !
+
+Quelle est la différence entre Jésus en vrai et Jésus en photo ? Il n'y a besoin que d'un clou pour accrocher Jésus en photo...
+
+Quel religieux résiste à toutes les infections ? L'abbé Tadine !
+
+Pourquoi les princes démons ne disent jamais rien d’intelligent quand il fait froid ? Parce que Baal hiverne !
+
+Que dit une fraise sur le dos d'un cheval ? Tagada, tagada,...
