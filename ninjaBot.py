76442c13b163ebc70d6559629b673617d72ddff6
Gabrielle Pauvert <gabrielle.pauvert@laposte.net>
2021-09-25 00:10:15 +0200

    correct configuration typo

--- ninjaBot.py
+++ ninjaBot.py
@@ -482,7 +482,7 @@ async def track(ctx, arg):
                 differences = d.make_file(old_pad.split("\n"), new_pad.split("\n"), context = True, numlines=2)
 
                 fileObj = io.StringIO(differences) #turns differences (string of a HTML file) into a valid file object
-                config = imgkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf', wkhtmltoimage='./bin/wkhtmltoimage')
+                config = imgkit.config(wkhtmltopdf='./bin/wkhtmltopdf', wkhtmltoimage='./bin/wkhtmltoimage')
                 img = silent(imgkit.from_file)(fileObj, False, config=config) #turns html file into an image
                 await msg.channel.send("Changement détecté sur le pad à l'url {} :".format(url))
                 await msg.channel.send(file=discord.File(io.BytesIO(img), filename = "diff.png"))
